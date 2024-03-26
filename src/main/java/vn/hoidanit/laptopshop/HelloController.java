0ed58da7a97288846c7fbd6150115e5898c3407b
linhthuynguyen612 <linhnguyenthuy612@gmail.com>
2024-03-27 02:00:09 +0700

    update

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,15 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+    return "Hello World anh Cường iu!";
+    }
+}
+
+
+

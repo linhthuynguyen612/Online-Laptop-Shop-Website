0abae89adaf330379a200e15a7841518c4d2e8b1
linhthuynguyen612 <linhnguyenthuy612@gmail.com>
2024-03-30 02:20:24 +0700

    Update SQL

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-    return "Hello World anh Cường iu!";
+    return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+    return "Only user can see this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+    return "Only admin can see this page!";
     }
 }
 

c0e30f6671602918cf9fafc09b0bdd73c986f327
linhthuynguyen612 <linhnguyenthuy612@gmail.com>
2024-03-30 15:18:57 +0700

    setup jsp

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,26 +7,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-    
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "home";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
-    
+
     private UserService userService;
     
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @GetMapping("")
+    
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+    
+    // private UserService userService;
+    
+    // public UserController(UserService userService) {
+    //     this.userService = userService;
+    // }
+
+//     @GetMapping("")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }

6f88fb1eeaa495586baa8e3b4380fa621fe6b259
linhthuynguyen612 <linhnguyenthuy612@gmail.com>
2024-04-07 16:58:44 +0700

    update login

--- src/main/java/vn/hoidanit/laptopshop/domain/dto/registerDTO.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/dto/registerDTO.java
@@ -1,13 +1,24 @@
 package vn.hoidanit.laptopshop.domain.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import vn.hoidanit.laptopshop.service.validator.RegisterChecked;
 
 @RegisterChecked
 public class registerDTO {
+
+    @Size(min=3, message = "firstName phải tối thiểu 3 kí tự")
     private String firstName;
     private String lastName;
+
+    @Email(message = "Email không hợp lệ", regexp =
+    "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Email không được để trống")
     private String email;
     private String password;
+
+    @Size(min=3, message = "confirmPassword phải tối thiểu 3 kí tự")
     private String confirmPassword;
 
     public String getFirstName() {

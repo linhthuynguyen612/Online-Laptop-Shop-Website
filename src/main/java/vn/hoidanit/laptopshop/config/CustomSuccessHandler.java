6a30bfc0943074337068ae7fc969f6cb726cecff
linhthuynguyen612 <linhnguyenthuy612@gmail.com>
2024-04-07 23:49:35 +0700

    update login page

---
+++ src/main/java/vn/hoidanit/laptopshop/config/CustomSuccessHandler.java
@@ -0,0 +1,55 @@
+package vn.hoidanit.laptopshop.config;
+
+import java.io.IOException;
+
+import java.util.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class CustomSuccessHandler implements AuthenticationSuccessHandler{
+    protected String determineTargetUrl(final Authentication authentication) {
+        Map<String, String> roleTargetUrlMap = new HashMap<>();
+        roleTargetUrlMap.put("ROLE_USER", "/");
+        roleTargetUrlMap.put("ROLE_ADMIN", "/admin");
+        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (final GrantedAuthority grantedAuthority : authorities) {
+            String authorityName = grantedAuthority.getAuthority();
+            if (roleTargetUrlMap.containsKey(authorityName)) {
+                return roleTargetUrlMap.get(authorityName);
+            }
+        }
+        throw new IllegalStateException();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute("SPRING_SECURITY_LAST_EXCEPTION");
+    }
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+        String targetUrl = determineTargetUrl(authentication);
+        if(response.isCommitted()){
+           
+            return;
+        }
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+        clearAuthenticationAttributes(request);
+    }
+    
+}
--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -1,15 +1,18 @@
 package vn.hoidanit.laptopshop.config;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
 import vn.hoidanit.laptopshop.service.UserService;
 
@@ -27,14 +30,40 @@ public class SecurityConfiguration {
         return new CustomUserDetailsService(userService);
     }
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http,
-            PasswordEncoder passwordEncoder,
-            UserDetailsService userDetailsService) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http
-            .getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder
-            .userDetailsService(userDetailsService)
-            .passwordEncoder(passwordEncoder);
-        return authenticationManagerBuilder.build();
+    public DaoAuthenticationProvider authProvider(
+                PasswordEncoder passwordEncoder,
+                UserDetailsService userDetailsService) {
+            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+            authProvider.setUserDetailsService(userDetailsService);
+            authProvider.setPasswordEncoder(passwordEncoder);
+            //authProvider.setHideUserNotFoundExceptions(false);
+            return authProvider;
     }
+
+    @Bean
+    public AuthenticationSuccessHandler customSuccessHandler() {
+        return new CustomSuccessHandler();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE) 
+            .permitAll()
+
+            .requestMatchers("/","/login", "/client/**","/product/**", "/css/**", "/js/**","/images/**")
+            .permitAll()
+
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+
+            .anyRequest().authenticated())
+
+        .formLogin(formLogin -> formLogin
+            .loginPage("/login")
+            .failureUrl("/login?error")
+            .successHandler(customSuccessHandler())
+            .permitAll());
+    return http.build();
+}
 }
